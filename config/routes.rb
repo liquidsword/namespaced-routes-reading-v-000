4c7e89d1a67dc2ef62eb6829c3352fe945358f2f
Auden Robertson <audenrobertson@gmail.com>
2018-06-03 01:14:21 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  scope '/admin' do
-  resources :stats, only: [:index]
-end
+  scope '/admin', module: 'admin' do
+    resources :stats, only: [:index]
+  end
 
   resources :authors, only: [:show, :index] do
     resources :posts, only: [:show, :index, :new, :edit]
